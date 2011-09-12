795bf891ccfa1368c01b3b8888dc1bfa61311810
Alexander Slipchenko <slipchenko.alexander@gmail.com>
2011-09-12 17:51:40 +0300

    proper quoting

--- test/test.sh
+++ test/test.sh
@@ -23,24 +23,24 @@ export BACKUP_EXCLUDE_TABLES
 BACKUP_FILES_ID=test
 export BACKUP_FILES_ID
 
-SRV_ID="$TEST_DIR/srv"
+SRV_ID=$TEST_DIR/srv
 
-TEST_DB="backup_test"
-MYSQL="mysql -uroot"
-USER="root"
-PASS="-"
+TEST_DB=backup_test
+MYSQL='mysql -uroot'
+USER=root
+PASS=-
 
 cleanup() {
-    if [[ -e "$TEST_DIR/tmp" ]]; then
+    if [[ -e $TEST_DIR/tmp ]]; then
         rm -r "$TEST_DIR/tmp"
     fi
-    if [[ -e "$TEST_DIR/$FILES_DIR" ]]; then
+    if [[ -e $TEST_DIR/$FILES_DIR ]]; then
         rm -r "$TEST_DIR/$FILES_DIR"
     fi
-    if [[ -e "$SRV_ID" ]]; then
+    if [[ -e $SRV_ID ]]; then
         rm -r "$SRV_ID"
     fi
-    if [[ -e "$BACKUP_TMP_DIR" ]]; then
+    if [[ -e $BACKUP_TMP_DIR ]]; then
         rm -r "$BACKUP_TMP_DIR"
     fi
     $MYSQL -e "DROP DATABASE IF EXISTS $TEST_DB"
@@ -67,11 +67,11 @@ restore_files() {
     local ARCHIVE=$2 # e.g. "test" or "test.inc"
     # don't produce error only when there is already a "restored-files" dir
     # i.e. this is different from "mkdir -p", because it will fail if e.g. "tmp" will not exist
-    if [[ ! -e "$TEST_DIR/tmp/restored-files" ]]; then
+    if [[ ! -e $TEST_DIR/tmp/restored-files ]]; then
         mkdir "$TEST_DIR/tmp/restored-files"
     fi
     if [[ $UID != 0 ]]; then
-        DAR_OPT="-O" # ignore owner (owner can be restored only under root)
+        DAR_OPT=-O # ignore owner (owner can be restored only under root)
     fi
     # -w means "don't warn when overwriting files" and it must be there 
     # if we are simply restoring an archive
@@ -108,13 +108,13 @@ check_db() {
 }
 
 check_ignore_tables() {
-    grep -vi "table1" "$TEST_DIR/tmp/restored-db.sql" > /dev/null
-    grep -vi "table2" "$TEST_DIR/tmp/restored-db.sql" > /dev/null
+    grep -vi table1 "$TEST_DIR/tmp/restored-db.sql" > /dev/null
+    grep -vi table2 "$TEST_DIR/tmp/restored-db.sql" > /dev/null
 }
 
-DATE0=`date -d "+0 days" -I`
-DATE1=`date -d "+1 days" -I`
-DATE2=`date -d "+2 days" -I`
+DATE0=`date -d '+0 days' -I`
+DATE1=`date -d '+1 days' -I`
+DATE2=`date -d '+2 days' -I`
 
 PATH="$PATH:$TEST_DIR/../src"
 
@@ -125,21 +125,21 @@ echo OK # initializing
 
 
 
-echo -n "making full backup... "
-use_files "test.0"
-use_db "backup_test.0.sql"
+echo -n 'making full backup... '
+use_files test.0
+use_db backup_test.0.sql
 DATE="$DATE0" BACKUP_OPTIONS="$TEST_DIR/config" backup "$SRV_ID" > /dev/null
 echo OK
 
-echo -n "making incremental backup against a full backup... "
-use_files "test.1"
-use_db "backup_test.1.sql"
+echo -n 'making incremental backup against a full backup... '
+use_files test.1
+use_db backup_test.1.sql
 DATE="$DATE1" BACKUP_OPTIONS="$TEST_DIR/config" backup-incr "$SRV_ID" > /dev/null
 echo OK
 
-echo -n "making incremental backup against an incremental backup... "
-use_files "test.2"
-use_db "backup_test.2.sql"
+echo -n 'making incremental backup against an incremental backup... '
+use_files test.2
+use_db backup_test.2.sql
 DATE="$DATE2" BACKUP_OPTIONS="$TEST_DIR/config" backup-incr "$SRV_ID" > /dev/null
 echo OK
 
@@ -147,44 +147,44 @@ echo OK
 
 
 echo checking full backup...
-echo -n "   files... "
+echo -n '   files... '
 restore_files "$DATE0" "$BACKUP_FILES_ID"
-check_files "test.0"
+check_files test.0
 echo OK
-echo -n "   db... "
+echo -n '   db... '
 restore_db "$DATE0"
-check_db "reference.0.sql"
+check_db reference.0.sql
 echo OK
-echo -n "   ignore-tables..." 
+echo -n '   ignore-tables...'
 check_ignore_tables
 echo OK
 
 
 
 echo checking 1st incremental backup...
-echo -n "   files... "
+echo -n '   files... '
 restore_files "$DATE1" "$BACKUP_FILES_ID.inc"
-check_files "test.1"
+check_files test.1
 echo OK
-echo -n "   db... "
+echo -n '   db... '
 restore_db_incr "$DATE1"
-check_db "reference.1.sql"
+check_db reference.1.sql
 echo OK
-echo -n "   ignore-tables..." 
+echo -n '   ignore-tables...' 
 check_ignore_tables
 echo OK
 
 
 echo checking 2nd incremental backup...
-echo -n "   files... "
+echo -n '   files... '
 restore_files "$DATE2" "$BACKUP_FILES_ID.inc"
-check_files "test.2"
+check_files test.2
 echo OK
-echo -n "   db... "
+echo -n '   db... '
 restore_db_incr "$DATE2"
-check_db "reference.2.sql"
+check_db reference.2.sql
 echo OK
-echo -n "   ignore-tables..." 
+echo -n '  ignore-tables...' 
 check_ignore_tables
 echo OK
 
