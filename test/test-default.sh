9c26f962ba746a762bf95d8d668f6f306f30f6c9
Alexander Slipchenko <slipchenko.alexander@gmail.com>
2011-09-21 15:03:33 +0300

    configurable test data dir, missing TEST_EXCLUDE_TABLES export

--- test/test-default.sh
+++ test/test-default.sh
@@ -8,11 +8,12 @@ PATH="$PATH:`readlink -f .`:`readlink -f ../src/bin`"; export PATH
 
 . "`backup-include util`"
 
+TEST_DATA_DIR='data-default'
 TEST_BACKUP_DIR='local_dir'; export TEST_BACKUP_DIR
 TEST_FILES_DIR='www'; export TEST_FILES_DIR # files to backup
 TEST_FILES_ALIAS='test'; export TEST_FILES_ALIAS
 TEST_EXCLUDE_TABLES='table2
-    table3' # newline intended
+    table3'; export TEST_EXCLUDE_TABLES # newline intended
 TEST_SERVER="`whoami`@localhost"; export TEST_SERVER
 TEST_SERVER_PATH="`abs_dirname "$0"`/srv"; export TEST_SERVER_PATH # server path should be absolute
 
