28c5cf2599606c36264ae9507a64717800f854b4
Alexander Slipchenko <slipchenko.alexander@gmail.com>
2011-09-21 14:43:53 +0300

    exclude tables instead of ignore

--- test/test.sh
+++ test/test.sh
@@ -4,14 +4,15 @@ set -e
 
 echo -n 'initializing... '
 
-PATH="$PATH:`readlink -f ../src/bin`"; export PATH 
+PATH="$PATH:`readlink -f .`:`readlink -f ../src/bin`"; export PATH 
 
 . "`backup-include util`"
 
 TEST_BACKUP_DIR='local_dir'; export TEST_BACKUP_DIR
 TEST_FILES_DIR='www'; export TEST_FILES_DIR # files to backup
 TEST_FILES_ALIAS='test'; export TEST_FILES_ALIAS
-TEST_IGNORE_TABLES='table2 table3'; export TEST_IGNORE_TABLES
+TEST_EXCLUDE_TABLES='table2
+    table3' # newline intended
 TEST_SERVER="`whoami`@localhost"; export TEST_SERVER
 TEST_SERVER_PATH="`abs_dirname "$0"`/srv"; export TEST_SERVER_PATH # server path should be absolute
 
@@ -24,6 +25,8 @@ TEST_PASS=''; export TEST_PASS
 MYSQL='mysql -uroot'
 
 cleanup() {
+    echo 'cleanup...'
+
     if [[ -e './tmp' ]]; then
         rm -r './tmp'
     fi
@@ -97,22 +100,20 @@ restore_db_incr() {
 
 check_db() {
     local reference_sql=$1
-    local opt=`ignore_table_opt "$TEST_DB" "$TEST_IGNORE_TABLES"`
-    backup_mysql_dump "$TEST_DB" "$TEST_USER" "$TEST_PASS" "$opt" './restored.sql'
+    backup_mysql_dump "$TEST_DB" "$TEST_USER" "$TEST_PASS" "$TEST_EXCLUDE_TABLES" '' './restored.sql'
     diff './restored.sql' "./data/$reference_sql"
     rm './restored.sql'
 }
 
 check_ignore_tables() {
-    grep -vi 'table1' './tmp/restored-db.sql' > /dev/null
+    # FIXME: does not work
     grep -vi 'table2' './tmp/restored-db.sql' > /dev/null
+    grep -vi 'table3' './tmp/restored-db.sql' > /dev/null
 }
 
 DATE0=`date -d '+0 days' -I`
 DATE1=`date -d '+1 days' -I`
 DATE2=`date -d '+2 days' -I`
-
-PATH="$PATH:../src/bin"
 
 mkdir "$TEST_SERVER_PATH"
 mkdir './tmp'
@@ -180,11 +181,9 @@ echo -n '   db... '
 restore_db_incr "$DATE2"
 check_db 'reference.2.sql'
 echo 'OK'
-echo -n '  ignore-tables...' 
+echo -n '   ignore-tables...' 
 check_ignore_tables
 echo 'OK'
-
-echo 'cleanup...'
 
 cleanup
 
